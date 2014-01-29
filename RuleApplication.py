20d25eeda5a90a76763a3038f041d13790e979bf
jkodner05 <jkodner05@yahoo.com>
2014-01-28 19:51:01 -0500

    comments

--- RuleApplication.py
+++ RuleApplication.py
@@ -10,6 +10,8 @@ UNDEF = 2
 PADDING = 2
 
 def get_features(feature_list, these_feature):
+    """Translate list of features in format "+/-name1 +/-name2 into
+    dictionary of features given full list of possible features"""
     features = {}
     def feat_filter(feature, this):
         try:
@@ -25,6 +27,9 @@ def get_features(feature_list, these_feature):
     return features
 
 def match_features(phone_feats, other_feats):
+    """Determine whether two sets of features match.
+    A phone is "matched" if every defined feature in the matching environment
+    is matches the feature in phone"""
     for feat in other_feats.keys():
         if phone_feats[feat] != other_feats[feat] and other_feats[feat] != UNDEF:
             return False
@@ -32,20 +37,25 @@ def match_features(phone_feats, other_feats):
            
 
 class Executor():
+    """Put words through phonological grammar to get their surface representations"""
 
     def __init__(self, grammar):
         self.grammar = grammar
 
     def getPhoneUR(self, char):        
+        """Get Phone matching character"""
         return Phone(copy(self.grammar.phones[self.grammar.phone_char_mappings[char]]), char, False)
 
     def getUR(self, flat_word):
+        """Get underlying representation of word in Phones"""
         center = [self.getPhoneUR(char) for char in flat_word]
         ends = [Phone(self.grammar.features,"#",boundary=True)]*PADDING*2
         ends[PADDING:PADDING] = center
         return ends
 
     def segs_match(self, segments, sylls, piv_index, word, syll_aware, is_prefix):
+        """try matching environment of a segment
+        may be syllable-aware (syllabification) or not"""
         if is_prefix:
             potential = [phone.features for phone in word[piv_index-len(segments):]]
             potential_sylls = sylls[piv_index-len(segments):]
@@ -59,9 +69,11 @@ class Executor():
         return True
 
     def clean_len(self, segment):
+        """Find the length of word minus padding"""
         return len(segment) - segment.count("#")
 
     def syllabify(self, word):
+        """Syllabify given word"""
         sylls = [-1]*len(word)
         moras = [False]*len(word)
         count = 0
@@ -100,6 +112,7 @@ class Executor():
         return
 
     def match_env(self, rule, word, index):
+        """given location of a matching phone  and a rule, check if the phone is in the environment"""
         sylls = [phone.syll for phone in word]
         if not self.segs_match(rule.pre_env,sylls,index,word,False,True):
             return False
@@ -108,17 +121,24 @@ class Executor():
         return True
     
     def match_rule_seg(self, rule, seg):
+        """determine if this phone matches the rule"""
         for feat in rule.seg_match.keys():
             if not match_features(seg.features,rule.seg_match):
                 return False
         return True
 
     def change_features(self,rule, seg): 
+        """change features of matched phone"""
         for feat in rule.seg_change.keys():
             if rule.seg_change[feat] != UNDEF:
                 seg.features[feat] = rule.seg_change[feat]
 
     def apply_rule(self, rule, word):
+        """Apply ordered rule to word.
+        Only a few types of rules currently supported
+        no deletions or insertions
+        not all expressions of environments are permissible yet
+        no syllable-aware rule environments"""
         for i, phone in enumerate(word):
             if self.match_rule_seg(rule,phone):
                 if self.match_env(rule,word,i):
@@ -126,6 +146,7 @@ class Executor():
         return word
 
     def get_char_representation(self, seg):
+        """Determine character representation of phone"""
         char_rep = seg.features.__str__()
         for phone in self.grammar.phones.keys():
             match = True
@@ -140,20 +161,22 @@ class Executor():
         return ''
 
     def get_word_representation(self, word):
+        """Output list of phones in readable format"""
         return "".join([self.get_char_representation(seg) for seg in word])
                     
 class GlobalGrammar():
+    """Processes config file to represent phonological grammar"""
 
     def __init__(self, filename):
         self.features = None
         self.phones = None
         self.syllables = None
         self.rules = None
-        self.abbrevs = None
         self.sections = self.section_file(filename)
 
 
     def section_file(self, filename):
+        """Divide config file into sections, parse each section"""
         f = open(filename, "r")
         in_feat = True
         full = f.read().split("!")
@@ -165,30 +188,28 @@ class GlobalGrammar():
         self.rules = self.parse_rules(sec_filter(full, "RULE"))
         return full
 
-    def reverse_abbrevs(self):
-        revs = {}
-        for key in self.abbrevs.keys():
-            revs[self.abbrevs[key]] = key
-        return revs
-
     def read_features(self, feature_sec):
+        """remove character names from list of binary features"""
         return [line.split(":")[1].strip() for line in 
                 filter(lambda x:
                            x and "FEATURE" not in x, feature_sec.split("\n"))]
 
     def map_phones(self, phone_sec, abbrev_sec):
+        """map phonemes, abbreviations, and word boundary (#) characters to dictionaries of binary feature values"""
         combined = phone_sec + abbrev_sec + "#: "
         return {line.split(":")[0].split()[-1].strip() : get_features(self.features,line.split(":")[1].strip()) for line in 
                 filter(lambda x:
                            x and "ABBREV" not in x and "PHONE" not in x, combined.split("\n"))}        
 
     def map_phone_chars(self, phone_sec, abbrev_sec):
-        combined = phone_sec + abbrev_sec + "#: "
+        """Some phones are represented by 2+ characters. Map those to single characters as specified in config file"""
+        combined = phone_sec + abbrev_sec
         return {line.split(":")[0].split()[-1].strip() : line.split(":")[0].split()[0].strip() for line in 
                 filter(lambda x:
                             x and "ABBREV" not in x and "PHONE" not in x, combined.split("\n"))}        
 
     def read_syllables(self, syll_sec):
+        """Read syllable formats, decompose into sets of nuclei (the vowel, probably), onsets (before the nucleus), and codas (after the nucleus)"""
         syllables = set([line.strip() for line in 
                 filter(lambda x:
                            x and "SYLL" not in x, syll_sec.split("\n"))])
@@ -199,6 +220,16 @@ class GlobalGrammar():
         return {'syllables':syllables,'onsets':onsets,'codas':codas}
 
     def parse_rules(self, rule_sec):
+        """Create a new rule for each rule line:
+        format:
+        A > B
+        A > B / C_
+        A > B / _D
+        A > B / C_D
+        Read as "The features of B are applied to any phone matching A in the environment after C and before D"
+        A, C, D may be phones, abbreviations, or sets of features
+        B may be a phone or set of features"""
+
         rules = set([line.strip() for line in 
                 filter(lambda x:
                            x and "RULE" not in x, rule_sec.split("\n"))])
@@ -206,15 +237,16 @@ class GlobalGrammar():
 
 
 class Rule(object):
+    """Representation of ordered rule"""
 
     def __init__(self, rule_str, features, abbrevs):
         self.rule_str = rule_str
         self.features = features
         rule_list = re.split('[/>_]',rule_str)
-        self.seg_match = self.get_seg_feats(rule_list[0].strip())
-        self.seg_change = self.get_seg_feats(rule_list[1].strip())
-        self.pre_env = rule_list[2].strip() if len(rule_list) > 2 else None
-        self.post_env = rule_list[3].strip() if len(rule_list) > 2 else None
+        self.seg_match = self.get_seg_feats(rule_list[0].strip()) # A
+        self.seg_change = self.get_seg_feats(rule_list[1].strip()) # B
+        self.pre_env = rule_list[2].strip() if len(rule_list) > 2 else None # C
+        self.post_env = rule_list[3].strip() if len(rule_list) > 2 else None # D
 
     def get_seg_feats(self, match_str):
         return get_features(self.features, re.sub("[\[|\]]","",match_str.strip()))
@@ -223,6 +255,7 @@ class Rule(object):
         return not (self.pre_env and self.post_env)
 
 class Phone(object):
+    """Representation of phone. Contains feature information, character representation information, and syllabification related information"""
 
     def __init__(self, features=None, phone=None, boundary=False):
         if not boundary:
@@ -239,6 +272,7 @@ class Phone(object):
             self.features = get_features(features, None)
 
     def __str__(self):
+        """Don't use this. Unicode mess"""
         mapped = self.mapped
         if not mapped:
             mapped = ""
